179a5194a08d4a637d67749d9b556a5fb96dc38e
Gayu-Ravi <114924827+Gayu-Ravi@users.noreply.github.com>
2022-10-12 11:47:41 +0530

    Add files via upload

---
+++ printmultiplelines.java
@@ -0,0 +1,10 @@
+class printmultiplelines
+{
+public static void main(String args[])
+{
+System.out.println("Not all classrooms hv 4 walls");
+System.out.println("Disconnect to receonnect");
+System.out.println("Wander where the wifi is weak");
+System.out.println("Live the moments u cant put into words");
+}
+}
---
+++ sumoftwonumbers.java
@@ -0,0 +1,7 @@
+class sumoftwonumbers
+{
+public static void main(String args[])
+{
+System.out.print(10+12);
+}
+}
