c877d7b572e978f3d7d8c900a229cc159a23bcc1
Gayu-Ravi <114924827+Gayu-Ravi@users.noreply.github.com>
2022-10-13 13:50:29 +0530

    Add files via upload

---
+++ Personaldetails.java
@@ -0,0 +1,59 @@
+class Personaldetails
+{
+public static void main(String args[])
+{
+String Name= "Gayathri";
+String Gender= "Female";
+String DOB="08/06/1998";
+int Age=24;
+String Maritalstatus="Unmarried";
+String Doornumber="792";
+String Street="Vellalar street";
+String Locality="Perungattur";
+String City="Cheyyar";
+String State="Tamilnadu";
+String Country="India";
+int Pincode=604402;
+String IsAadharAvailable="Yes";
+String IsTelephoneAvailable="Yes";
+long PhoneNumber=8531800610L;
+String Qualification="B.E.Civil";
+String IsSalaried="No";
+String MonthlySalary="-";
+String MonthlyExpenses="-";
+String Hobbies="Reading,Healing";
+System.out.println("Name\t\t\t:"+Name);
+System.out.println("Gender\t\t\t:"+Gender);
+System.out.println("DOB\t\t\t:"+DOB);
+System.out.println("Age\t\t\t:"+Age);
+System.out.println("Marital status\t\t:"+Maritalstatus);
+System.out.println("Door number\t\t:"+Doornumber);
+System.out.println("Street\t\t\t:"+Street);
+System.out.println("Locality\t\t:"+Locality);
+System.out.println("City\t\t\t:"+City);
+System.out.println("State\t\t\t:"+State);
+System.out.println("Country\t\t\t:"+Country);
+System.out.println("Pincode\t\t\t:"+Pincode);
+System.out.println("IsAadharAvailable       :"+IsAadharAvailable);
+System.out.println("IsTelephoneAvailable    :"+IsTelephoneAvailable);
+System.out.println("Phone Number\t\t:"+PhoneNumber);
+System.out.println("Qualification\t\t:"+Qualification);
+System.out.println("IsSalaried\t\t:"+IsSalaried);
+System.out.println("Monthly Salary\t\t:"+MonthlySalary);
+System.out.println("Monthly Expenses\t:"+MonthlyExpenses);
+System.out.println("Hobbies\t\t\t:"+Hobbies);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
