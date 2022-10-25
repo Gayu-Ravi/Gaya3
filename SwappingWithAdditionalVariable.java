f654adbcab65c45fe95384a4a6331af6ee31b47f
Gayu-Ravi <114924827+Gayu-Ravi@users.noreply.github.com>
2022-10-25 10:29:02 +0530

    Update SwappingWithAdditionalVariable.java

--- SwappingWithAdditionalVariable.java
+++ SwappingWithAdditionalVariable.java
@@ -2,11 +2,19 @@ class SwappingWithAdditionalVariable
 {
 	public static void main(String args[])
 	{
-		int a=86;
-		int b=1011;
-		int c=a;
-		System.out.println("a="+b);
-		System.out.println("b="+c);
+	int a = 86, y = 1011;
+ 
+        System.out.println("Before Swap");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+ 
+        int x = a;
+        a = b;
+        b = x;
+ 
+        System.out.println("After swap");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
 	}
 }
 	 
