b07e89ff4d16d6eb33f7e669b77f71e6047b4279
Gayu-Ravi <114924827+Gayu-Ravi@users.noreply.github.com>
2022-10-26 20:31:21 +0530

    Update TypeConversion6.java

--- TypeConversion6.java
+++ TypeConversion6.java
@@ -2,16 +2,24 @@ public class TypeConversion6
 {
 	public static void main(String args[])
 	{	
+		
 		System.out.println("Int to Boolean Conversion");
-		int a=1;
+		int a=1, r=234;
 		boolean b=(a==1);
+		boolean p=(r==234);
 		System.out.println("Given value :" + a);
 		System.out.println("Result      :" + b);
+		System.out.println("Given value :" + r);
+		System.out.println("Result      :" + p);
 		
 		System.out.println("Boolean to Int Conversion");
 		boolean g=true;
 		int d=(g) ? 1 : 0 ;
+		boolean e=false;
+		int f=(e)?1:0;
 		System.out.println("Given value :" + g);
 		System.out.println("Result      :" + d);
+		System.out.println("Given value :" + e);
+		System.out.println("Result      :" + f);
 	}
 }
