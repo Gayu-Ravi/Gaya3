a35aa3282eab27ad87840bc41cad402035e9a09b
Gayu-Ravi <114924827+Gayu-Ravi@users.noreply.github.com>
2022-10-28 12:18:02 +0530

    Add files via upload

--- TypeConversion6.java
+++ TypeConversion6.java
@@ -2,19 +2,18 @@ public class TypeConversion6
 {
 	public static void main(String args[])
 	{	
-		
 		System.out.println("Int to Boolean Conversion");
 		int a=1, r=234;
-		boolean b=(a==1);
-		boolean p=(r==234);
+		boolean b=(boolean) a; //incompatible types: int cannot be converted to boolean
+                boolean p=(boolean) r;
 		System.out.println("Given value :" + a);
 		System.out.println("Result      :" + b);
 		System.out.println("Given value :" + r);
 		System.out.println("Result      :" + p);
 		
 		System.out.println("Boolean to Int Conversion");
-		boolean g=true;
-		int d= (int) g ;
+		boolean g=true; //incompatible types: boolean cannot be converted to int
+		int d= (int) g;
 		boolean e=false;
 		int f=(int) e;
 		System.out.println("Given value :" + g);
--- TypeConversion7.java
+++ TypeConversion7.java
@@ -2,15 +2,15 @@ public class TypeConversion7
 {
 	public static void main(String args[])
 	{	
-		System.out.println("Char to String Conversion");
+		System.out.println("Char to String Conversion");// incompatible types: char cannot be converted to String
 		char a='R';
-		String b=Character.toString(a);
+		String b=(String) a;
 		System.out.println("Given value :" + a);
 		System.out.println("Result      :" + b);
 		
-		System.out.println("String to Char Conversion");
+		System.out.println("String to Char Conversion");// incompatible types: String cannot be converted to char
 		String g="RAVI";
-		char d=g.charAt(0);
+		char d=(char) g;
 		System.out.println("Given value :" + g);
 		System.out.println("Result      :" + d);
 	}
